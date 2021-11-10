fa5442816777b9a8dc1c8cae1b27081aac55dfbf
Giberson Lara <giberlara@gmail.com>
2021-11-09 23:57:35 -0400

    origins change

--- config/middleware.js
+++ config/middleware.js
@@ -12,7 +12,8 @@ module.exports = {
     },
     cors: {
       enabled: true,
-      origin: ['*']
+      headers: '*',
+      origin: ['http://localhost:3000', 'http://192.168.1.53:3000', 'http://localhost']
     },
   }
 }

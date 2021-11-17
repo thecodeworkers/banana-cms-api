1fcf09a53eab2d2af9d11be7966bda796184e69a
Giberson Lara <giberlara@gmail.com>
2021-11-17 16:05:21 -0400

    middleware config

--- config/middleware.js
+++ config/middleware.js
@@ -11,9 +11,26 @@ module.exports = ({ env }) => ({
       }
     },
     cors: {
-      enabled: false,
-      headers: '*',
+      enabled: true,
       origin: ['http://localhost:3000', 'http://localhost', env('URL', 'http://localhost:1337')],
+      expose: ['WWW-Authenticate', 'Server-Authorization', 'Access-Control-Expose-Headers'],
+      maxAge: 31536000,
+      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+      headers: [
+        'Content-Type',
+        'Authorization',
+        'X-Frame-Options',
+        'Origin',
+        'Access-Control-Allow-Headers',
+        'access-control-allow-origin',
+      ],
     },
   }
 })
+
+  // cors: {
+    //   enabled: false,
+    //   headers: '*',
+    //   origin: ['http://localhost:3000', 'http://localhost', env('URL', 'http://localhost:1337')],
+    // },

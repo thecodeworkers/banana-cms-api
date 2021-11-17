12957f4cfe29bc13e1eb90ece8b13ab63f7feac8
Giberson Lara <giberlara@gmail.com>
2021-11-17 17:09:30 -0400

    simplest way

--- config/middleware.js
+++ config/middleware.js
@@ -11,26 +11,8 @@ module.exports = ({ env }) => ({
       }
     },
     cors: {
-      enabled: true,
-      origin: ['http://localhost:3000', 'http://localhost', env('URL', 'http://localhost:1337')],
-      expose: ['WWW-Authenticate', 'Server-Authorization', 'Access-Control-Expose-Headers'],
-      credentials: false,
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
-      headers: [
-        'Content-Type',
-        'content-type',
-        'Authorization',
-        'X-Frame-Options',
-        'Origin',
-        'Access-Control-Allow-Headers',
-        'access-control-allow-origin',
-      ],
+      origin: [ 'http://localhost', 'https://banana-dev-web.thecodeworkers.com/', env('URL', 'http://localhost:1337') ]
     },
   }
 })
 
-  // cors: {
-    //   enabled: false,
-    //   headers: '*',
-    //   origin: ['http://localhost:3000', 'http://localhost', env('URL', 'http://localhost:1337')],
-    // },

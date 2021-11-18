8622a73d116c09acfcb26c1b5f84e62a5f762174
Jorge Bastidas <jorgebastidas9@gmail.com>
2021-11-17 21:28:25 -0400

    normalized

--- config/middleware.js
+++ config/middleware.js
@@ -1,4 +1,4 @@
-module.exports = ({ env }) => ({
+module.exports = {
   load: {
     before: ['responseTime', 'boom', 'logger', 'cors', 'responses', 'gzip'],
     after: ['parser', 'router'],
@@ -9,23 +9,7 @@ module.exports = ({ env }) => ({
       options: {
         br: false
       }
-    },
-    cors: {
-      enabled: true,
-      origin: ['http://localhost', 'http://localhost:3000', env('URL', 'http://localhost:1337'), 'https://banana-dev-web.thecodeworkers.com/'],
-      expose: ['WWW-Authenticate', 'Server-Authorization', 'Access-Control-Expose-Headers'],
-      credentials: false,
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
-      headers: [
-        'Content-Type',
-        'content-type',
-        'Authorization',
-        'X-Frame-Options',
-        'Origin',
-        'Access-Control-Allow-Headers',
-        'access-control-allow-origin',
-      ],
-    },
+    }
   }
-})
+}
 

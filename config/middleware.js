652864b9ff75ee890d6515418476399e1ab5e834
Jorge Bastidas <jorgebastidas9@gmail.com>
2022-04-11 11:27:17 -0400

    update

--- config/middleware.js
+++ config/middleware.js
@@ -9,7 +9,23 @@ module.exports = {
       options: {
         br: false
       }
-    }
+    },
+    cors: {
+      enabled: true,
+      origin: ['http://localhost', 'http://localhost:3000', env('URL', 'http://localhost:1337'), 'https://banana-dev-web.thecodeworkers.com/'],
+      expose: ['WWW-Authenticate', 'Server-Authorization', 'Access-Control-Expose-Headers'],
+      credentials: false,
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+      headers: [
+        'Content-Type',
+        'content-type',
+        'Authorization',
+        'X-Frame-Options',
+        'Origin',
+        'Access-Control-Allow-Headers',
+        'access-control-allow-origin',
+      ],
+    },
   }
 }
 
